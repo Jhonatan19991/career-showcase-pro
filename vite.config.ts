45214ac15c7b9ae3f1dff9bf0ba5c8045cea2d89
Jhonatan Morales <125423031+Jhonatan19991@users.noreply.github.com>
2025-06-18 20:45:10 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/career-showcase-pro/", // 👈 MUY IMPORTANTE para GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
